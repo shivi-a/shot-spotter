a370ab795f79f02d324ff23475b44ad1be39ba79
Shivani Aggarwal <saggarwal@college.harvard.edu>
2019-04-22 00:09:52 -0400

    Create dynamic leaflet map to showcase gunshot data at different time points; write data to RDS that can be read in by the Shiny App

--- shot-spotter.R
+++ shot-spotter.R
@@ -1,18 +1,32 @@
 # Dear Diary
 
 library(tidyverse)
-library(janitor)
-library(fs)
-library(tigris)
-library(ggthemes)
+library(leaflet)
+#library(sf)
+#library(tigris)
+#library(ggthemes)
+#library(gganimate)
 
-shot <- read_csv(file = "shot-spotter/camdencounty_nj.csv")
+shot <- read_csv(file = "shot-spotter/camdencounty_nj.csv") %>% mutate(type = fct_recode(type, "Multiple Gunshots" = "Multiple_Gunshots",
+                           "Single Gunshot" = "Single_Gunshot",
+                           "Gunshot or Firecracker" = "Gunshot_or_Firecracker"))
 
-camden <- tracts("NJ", "Camden", class="sf")
+shot_file <- tempfile()
+
+write_rds(shot, "shot-spotter/shot_file.rds")
+
+camden <- places("NJ", class = "sf") %>% filter(NAME == "Camden")
+
+shot_sf <- st_as_sf(shot, 
+                    coords = c("longitude", "latitude"), 
+                    crs = st_crs(camden)) 
 
 camden %>% 
   ggplot() + 
   geom_sf() +
-  labs(title = "Gunshots in Camden, NJ")+
+  geom_sf(data = shot_sf, aes(color = type, fill = type), alpha = 0.5) +
+  labs(title = "Gunshots in Camden, NJ | Year: {closest_state}") +
   coord_sf(crs = 5070, datum = NA) +
-  theme_map()
+  theme_map() +
+  theme(legend.position = "bottom") +
+  transition_states(yearmonth, state_length = 1.5)
--- shot-spotter/app.R
+++ shot-spotter/app.R
@@ -1,39 +1,75 @@
 
 library(shiny)
+library(tidyverse)
+library(leaflet)
+
+shot <- read_rds("shot_file.rds")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Gunshots in Camden City, NJ", "Data"),
    
+   br(),
+     
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
+        radioButtons("Year", 
+                     "Year:", 
+                     c(2013, 2014, 2015, 2016, 2017),
+                     selected = 2013),
+        
+         sliderInput("Month",
+                     "Month:",
                      min = 1,
-                     max = 50,
-                     value = 30)
+                     max = 12,
+                     value = 9),
+        
+        p("Note: ShotSpotter data available beginning 09/2013"),
+        br(),
+        
+        checkboxGroupInput("Type",
+                           "Type:",
+                           unique(shot$type), 
+                           selected = unique(shot$type))
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         h6("Thanks to data from the ", a("ShotSpotter Project", href="http://justicetechlab.org/shotspotter-data/"), " by the Justice Tech Lab, we can visualize gunshot data in Camden, NJ"),
+         leafletOutput("mapPlot")
       )
    )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw a Leaflet plot
+
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  
+   output$mapPlot <- renderLeaflet({
+     
+     shot_select <- shot %>% filter(year == input$Year, month == input$Month, type == input$Type)
+     
+     pal <- colorFactor(palette = c("red", "blue", "green"), 
+                        levels = c("Multiple Gunshots", "Single Gunshot", "Gunshot or Firecracker"))
+     
+     leaflet(options = leafletOptions(minZoom = 12, dragging = TRUE))  %>% 
+       addProviderTiles("Esri.WorldStreetMap")  %>% 
+       setView(lng = -75.119621, lat = 39.931364, zoom = 13) %>% 
+       addCircleMarkers(
+         data = shot_select, 
+         color = ~pal(shot_select$type), 
+         lng = shot_select$longitude, 
+         lat = shot_select$latitude, 
+         radius = 1, 
+         label = shot_select$date) %>% 
+       
+       setMaxBounds(lng1 = -75.119621 + .075, 
+                    lat1 = 39.931364 + .05, 
+                    lng2 = -75.119621 - .075, 
+                    lat2 = 39.931364 - .05)
    })
 }
 
