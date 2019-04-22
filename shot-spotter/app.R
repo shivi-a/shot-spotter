9dc62d0f5ed8e27d72925b6be0152a25a0040592
Shivani Aggarwal <saggarwal@college.harvard.edu>
2019-04-22 14:41:18 -0400

    Added explanatory comments to explain process for UI and server logic in the Shiny App

--- shot-spotter/app.R
+++ shot-spotter/app.R
@@ -6,11 +6,15 @@ library(tidyverse)
 library(leaflet)
 library(readr)
 
-# Read in Camden shotspotter data from rds file created from R script
+# Read in Camden shotspotter data from rds file created from R script in the
+# repo - this way data is processed separately, and then simply displayed in the
+# Shiny App
 
 shot <- read_rds("shot_file.rds")
 
-# Define UI for application that draws a histogram
+# Define UI for application that has an interactive Leaflet map of Camden
+# displaying gunshot data - with a play button that can be pressed to
+# automatically cycle through the month, creating an animation
 
 ui <- fluidPage(
    
@@ -57,6 +61,12 @@ ui <- fluidPage(
         
         br(),
         
+        # Check box selector for gun shot type so that viewers can choose one
+        # year at a type of gunshot fire (e.g. perhaps only the distribution of
+        # multiple ghunshots over time are of interest). The checkbox input type
+        # was chosen however, so that multiple types of gunsot fire can be
+        # visualized and constrasted
+        
         checkboxGroupInput("Type",
                            "Type:",
                            unique(shot$type), 
@@ -74,6 +84,9 @@ ui <- fluidPage(
         # site!)
         
         h6("Thanks to data from the ", a("ShotSpotter Project", href="http://justicetechlab.org/shotspotter-data/"), " by the Justice Tech Lab, we can visualize gunshot data in Camden, NJ"),
+        
+        # Include the Leaflet map output
+        
         leafletOutput("mapPlot")
       )
    )
@@ -85,10 +98,26 @@ server <- function(input, output, session) {
   
    output$mapPlot <- renderLeaflet({
      
+     # If no gunshot type is selected (length of input is zero), then display
+     # the blank map of Camden with the same viewing parameters
+     
+     # Or, if the year 2013 is selected and a month smaller than 05 is selected,
+     # then also display a blank map of Camden -- Shotspotter data for this city
+     # only begins after 05/2013, and adding this condition as well as the
+     # alternative outcome (a blank map) - prevents the app from throwing up an
+     # ugly error
+     
      if (length(input$Type) == 0 | (input$Year == 2013 & input$Month < 5)) {
-        
+     
+       # Create leaflet map with a minimum zoom of 13, focused on Camden, but
+       # draggable within a small range around Camden
+       
        leaflet(options = leafletOptions(minZoom = 13, dragging = TRUE))  %>% 
          addProviderTiles("Esri.WorldStreetMap")  %>% 
+         
+         # Used Google Maps to place a pin and determine ideal coordinates of
+         # Camden City around which to center the map
+         
          setView(lng = -75.119621, lat = 39.931364, zoom = 13) %>% 
          
          # Fix the bounds to which the leaflet map may be dragged, such that it is
@@ -103,14 +132,32 @@ server <- function(input, output, session) {
      
      else {
      
+     # Subset the shot data based upon the user's selections so that it informs
+     # the rendering of the map
+       
      shot_select <- shot %>% filter(year == input$Year, month == input$Month, type %in% input$Type)
+     
+     # Create a custom color palette to assign different colors to the different
+     # factor levels for gunshot type so that they will be distinguishable on
+     # the map
      
      pal <- colorFactor(palette = c("red", "blue", "green"), 
                         levels = c("Multiple Gunshots", "Single Gunshot", "Gunshot or Firecracker"))
      
+     # Create leaflet map with a minimum zoom of 13, focused on Camden, but
+     # draggable within a small range around Camden
+     
      leaflet(options = leafletOptions(minZoom = 13, dragging = TRUE))  %>% 
        addProviderTiles("Esri.WorldStreetMap")  %>% 
+       
+       # Used Google Maps to place a pin and determine ideal coordinates of
+       # Camden City around which to center the map
+       
        setView(lng = -75.119621, lat = 39.931364, zoom = 13) %>% 
+       
+       # Add markers using the data just from the subset of the shot data that
+       # corresponds to the user's selection on the sidepanel
+       
        addCircleMarkers(
          data = shot_select, 
          
@@ -144,5 +191,6 @@ server <- function(input, output, session) {
 }
 
 # Run the application 
+
 shinyApp(ui = ui, server = server)
 
