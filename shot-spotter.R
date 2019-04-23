e885c899c4871f06c5f4ecd1605489e0099a5f3f
Shivani Aggarwal <saggarwal@college.harvard.edu>
2019-04-22 22:33:32 -0400

    Cleaned up R script that reads in CSV file and produces RDS file for the Shiny App

--- shot-spotter.R
+++ shot-spotter.R
@@ -1,32 +1,18 @@
-# Dear Diary
+
+# Load neccessary dependencies
 
 library(tidyverse)
-library(leaflet)
-#library(sf)
-#library(tigris)
-#library(ggthemes)
-#library(gganimate)
+
+# Read in shot file and modify factor levels of type for plotting
 
 shot <- read_csv(file = "camdencounty_nj.csv") %>% mutate(type = fct_recode(type, "Multiple Gunshots" = "Multiple_Gunshots",
                            "Single Gunshot" = "Single_Gunshot",
                            "Gunshot or Firecracker" = "Gunshot_or_Firecracker"))
 
+# Create temporary file as precursor to RDS
+
 shot_file <- tempfile()
 
+# Write data originally from CSV to RDS file for manipulation by the Shiny App
+
 write_rds(shot, "shot-spotter/shot_file.rds")
-
-camden <- places("NJ", class = "sf") %>% filter(NAME == "Camden")
-
-shot_sf <- st_as_sf(shot, 
-                    coords = c("longitude", "latitude"), 
-                    crs = st_crs(camden)) 
-
-camden %>% 
-  ggplot() + 
-  geom_sf() +
-  geom_sf(data = shot_sf, aes(color = type, fill = type), alpha = 0.5) +
-  labs(title = "Gunshots in Camden, NJ | Year: {closest_state}") +
-  coord_sf(crs = 5070, datum = NA) +
-  theme_map() +
-  theme(legend.position = "bottom") +
-  transition_states(yearmonth, state_length = 1.5)
